fb5d4e61c444e9c734526d5b33370c622e3e4bfa
Muhamad Azamy <muhamad@incubaid.com>
2022-11-22 14:50:39 +0100

    review

--- node_test.go
+++ node_test.go
@@ -3,7 +3,6 @@ package substrate
 import (
 	"testing"
 
-	"github.com/centrifuge/go-substrate-rpc-client/v4/types"
 	"github.com/stretchr/testify/require"
 )
 
@@ -19,43 +18,15 @@ func TestNode(t *testing.T) {
 
 	farmID, twinID := assertCreateFarm(t, cl)
 
-	t.Run("TestCreateNode", func(t *testing.T) {
-		nodeID, err = cl.CreateNode(identity,
-			Node{
-				FarmID: types.U32(farmID),
-				TwinID: types.U32(twinID),
-				Location: Location{
-					City:      "SomeCity",
-					Country:   "SomeCountry",
-					Latitude:  "51.049999",
-					Longitude: "3.733333",
-				},
-				Resources: Resources{
-					HRU: 9001778946048,
-					SRU: 5121101905921,
-					CRU: 24,
-					MRU: 202802929664,
-				},
-				BoardSerial: OptionBoardSerial{
-					HasValue: true,
-					AsValue:  "some_serial",
-				},
-			},
-		)
-		require.NoError(t, err)
-	})
+	nodeID = assertCreateNode(t, cl, farmID, twinID, identity)
 
-	t.Run("TestGetNode", func(t *testing.T) {
-		node, err = cl.GetNode(nodeID)
-		require.NoError(t, err)
-		require.Equal(t, twinID, uint32(node.TwinID))
-		require.Equal(t, farmID, uint32(node.FarmID))
-	})
+	node, err = cl.GetNode(nodeID)
+	require.NoError(t, err)
+	require.Equal(t, twinID, uint32(node.TwinID))
+	require.Equal(t, farmID, uint32(node.FarmID))
 
-	t.Run("TestGetNodeByTwinID", func(t *testing.T) {
-		nodeID, err = cl.GetNodeByTwinID(uint32(node.TwinID))
-		require.NoError(t, err)
-		require.Equal(t, uint32(node.ID), nodeID)
-	})
+	nodeID, err = cl.GetNodeByTwinID(uint32(node.TwinID))
+	require.NoError(t, err)
+	require.Equal(t, uint32(node.ID), nodeID)
 
 }
