3d642a1ebf8e128cfe74e26e0b97fd9d0d5f5c77
renauter <renauter@gmail.com>
2022-11-21 22:53:45 -0300

    chore: contract test formatting

--- contract_test.go
+++ contract_test.go
@@ -9,6 +9,7 @@ import (
 
 func TestNameContract(t *testing.T) {
 	var contractID uint64
+	var nameContractID uint64
 
 	cl := startLocalConnection(t)
 	defer cl.Close()
@@ -24,7 +25,7 @@ func TestNameContract(t *testing.T) {
 	})
 
 	t.Run("TestGetContractIDByNameRegistration", func(t *testing.T) {
-		nameContractID, err := cl.GetContractIDByNameRegistration(testName)
+		nameContractID, err = cl.GetContractIDByNameRegistration(testName)
 		require.NoError(t, err)
 		require.Equal(t, contractID, nameContractID)
 	})
@@ -37,7 +38,9 @@ func TestNameContract(t *testing.T) {
 }
 
 func TestNodeContract(t *testing.T) {
+	var nodeID uint32
 	var contractID uint64
+	var contractIDWithHash uint64
 	var contract *Contract
 
 	cl := startLocalConnection(t)
@@ -49,26 +52,28 @@ func TestNodeContract(t *testing.T) {
 	farmID, twinID := assertCreateFarm(t, cl)
 
 	t.Run("TestCreateNodeContract", func(t *testing.T) {
-		nodeID, err := cl.CreateNode(identity, Node{
-			FarmID: types.U32(farmID),
-			TwinID: types.U32(twinID),
-			Location: Location{
-				City:      "SomeCity",
-				Country:   "SomeCountry",
-				Latitude:  "51.049999",
-				Longitude: "3.733333",
+		nodeID, err = cl.CreateNode(identity,
+			Node{
+				FarmID: types.U32(farmID),
+				TwinID: types.U32(twinID),
+				Location: Location{
+					City:      "SomeCity",
+					Country:   "SomeCountry",
+					Latitude:  "51.049999",
+					Longitude: "3.733333",
+				},
+				Resources: Resources{
+					HRU: 9001778946048,
+					SRU: 5121101905921,
+					CRU: 24,
+					MRU: 202802929664,
+				},
+				BoardSerial: OptionBoardSerial{
+					HasValue: true,
+					AsValue:  "some_serial",
+				},
 			},
-			Resources: Resources{
-				HRU: 9001778946048,
-				SRU: 5121101905921,
-				CRU: 24,
-				MRU: 202802929664,
-			},
-			BoardSerial: OptionBoardSerial{
-				HasValue: true,
-				AsValue:  "some_serial",
-			},
-		})
+		)
 		require.NoError(t, err)
 
 		contractID, err = cl.CreateNodeContract(identity, nodeID, "", "", 0, nil)
@@ -81,7 +86,7 @@ func TestNodeContract(t *testing.T) {
 	})
 
 	t.Run("TestGetContractWithHash", func(t *testing.T) {
-		contractIDWithHash, err := cl.GetContractWithHash(uint32(
+		contractIDWithHash, err = cl.GetContractWithHash(uint32(
 			contract.ContractType.NodeContract.Node),
 			contract.ContractType.NodeContract.DeploymentHash)
 
@@ -94,6 +99,7 @@ func TestNodeContract(t *testing.T) {
 }
 
 func TestGetRentContract(t *testing.T) {
+	var nodeID uint32
 	var contractID uint64
 
 	cl := startLocalConnection(t)
@@ -105,26 +111,28 @@ func TestGetRentContract(t *testing.T) {
 	farmID, twinID := assertCreateFarm(t, cl)
 
 	t.Run("TestCreateRentContract", func(t *testing.T) {
-		nodeID, err := cl.CreateNode(identity, Node{
-			FarmID: types.U32(farmID),
-			TwinID: types.U32(twinID),
-			Location: Location{
-				City:      "SomeCity",
-				Country:   "SomeCountry",
-				Latitude:  "51.049999",
-				Longitude: "3.733333",
+		nodeID, err = cl.CreateNode(identity,
+			Node{
+				FarmID: types.U32(farmID),
+				TwinID: types.U32(twinID),
+				Location: Location{
+					City:      "SomeCity",
+					Country:   "SomeCountry",
+					Latitude:  "51.049999",
+					Longitude: "3.733333",
+				},
+				Resources: Resources{
+					HRU: 9001778946048,
+					SRU: 5121101905921,
+					CRU: 24,
+					MRU: 202802929664,
+				},
+				BoardSerial: OptionBoardSerial{
+					HasValue: true,
+					AsValue:  "some_serial",
+				},
 			},
-			Resources: Resources{
-				HRU: 9001778946048,
-				SRU: 5121101905921,
-				CRU: 24,
-				MRU: 202802929664,
-			},
-			BoardSerial: OptionBoardSerial{
-				HasValue: true,
-				AsValue:  "some_serial",
-			},
-		})
+		)
 		require.NoError(t, err)
 
 		contractID, err = cl.CreateRentContract(identity, nodeID, nil)
