275ad4f80ca99f4257fdf3eb5ab8f8fad00559c8
renauter <renauter@gmail.com>
2022-11-29 18:53:22 -0300

    test: update style

--- service_contract_test.go
+++ service_contract_test.go
@@ -10,39 +10,39 @@ func TestServiceContract(t *testing.T) {
 	cl := startLocalConnection(t)
 	defer cl.Close()
 
-	service_id, err := NewIdentityFromSr25519Phrase(AliceMnemonics)
+	serviceID, err := NewIdentityFromSr25519Phrase(AliceMnemonics)
 	require.NoError(t, err)
 
-	consumer_id, err := NewIdentityFromSr25519Phrase(BobMnemonics)
+	consumerID, err := NewIdentityFromSr25519Phrase(BobMnemonics)
 	require.NoError(t, err)
 
 	// service_twinID := assertCreateTwin(t, cl, AliceMnemonics, AliceAddress)
 	// consumer_twinID := assertCreateTwin(t, cl, BobMnemonics, BobAddress)
 
-	service_account, err := FromAddress(AliceAddress)
+	serviceAccount, err := FromAddress(AliceAddress)
 	require.NoError(t, err)
 
-	consumer_account, err := FromAddress(BobAddress)
+	consumerAccount, err := FromAddress(BobAddress)
 	require.NoError(t, err)
 
-	contract, err := cl.ServiceContractCreate(service_id, service_account, consumer_account)
+	serviceContractID, err := cl.ServiceContractCreate(serviceID, serviceAccount, consumerAccount)
 	require.NoError(t, err)
 
 	metadata := "some_metadata"
-	err = cl.ServiceContractSetMetadata(service_id, contract, metadata)
+	err = cl.ServiceContractSetMetadata(consumerID, serviceContractID, metadata)
 	require.NoError(t, err)
 
-	var base_fee uint64 = 100
-	var variable_fee uint64 = 100
-	err = cl.ServiceContractSetFees(service_id, contract, base_fee, variable_fee)
+	var baseFee uint64 = 100
+	var variableFee uint64 = 100
+	err = cl.ServiceContractSetFees(serviceID, serviceContractID, baseFee, variableFee)
 	require.NoError(t, err)
 
-	err = cl.ServiceContractApprove(service_id, contract)
+	err = cl.ServiceContractApprove(serviceID, serviceContractID)
 	require.NoError(t, err)
 
-	err = cl.ServiceContractApprove(consumer_id, contract)
+	err = cl.ServiceContractApprove(serviceID, serviceContractID)
 	require.NoError(t, err)
 
-	err = cl.ServiceContractCancel(service_id, contract)
+	err = cl.ServiceContractCancel(consumerID, serviceContractID)
 	require.NoError(t, err)
 }
